df7f786c6cb53400fcfb92c855112dcadc1cc3cf
Kenneth Kinyanjui <kenmbugua@gmail.com>
2015-11-27 18:20:23 +0300

    configuring the routes for the api

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from quickstart.views import UserViewSet , GroupViewSet
+
+router =routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
